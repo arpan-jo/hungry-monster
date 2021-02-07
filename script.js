8125a74ac85e32e61fe72868e022b27555c6f147
arpan-jo <arpanjorng@gmail.com>
2021-02-07 18:24:42 +0600

    some changed

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
-	.then(response => response.json())
-	// .then(data => console.log(data.meals));
-	.then(data => showFood(data.meals));
-
+document.getElementById('search').addEventListener('click', function () {
+	const names = document.getElementById('foodSearch');
+	foodSearch = names.value;
+	fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodSearch}`)
+		.then(response => response.json())
+		.then(data => showFood(data.meals));
+	document.getElementById('foodSearch').value = '';
+});
 const showFood = foods => {
 	foods.forEach(elements => {
-		// console.log(elements);
 		const container = document.getElementById('food');
 		const foodContainer = document.createElement('div');
-		const foodName = document.createElement('div');
-		foodName.className = 'image-container';
-		foodName.innerHTML = `<img id="images" class="image" src="${elements.strMealThumb}" alt="Food Image">
-		<p class="food-name">${elements.strMeal}</p>
-		<div id="ingredients">
-			<p>Ingredients</p>
-			<ul>
-				<li>${elements.strIngredient1}</li>
-				<li>${elements.strIngredient2}</li>
-				<li>${elements.strIngredient3}</li>
-				<li>${elements.strIngredient4}</li>
-				<li>${elements.strIngredient5}</li>
-				<li>${elements.strIngredient6}</li>
-			</ul>
-		</div>
-	`;
-		// document.getElementById('ingredients').style.display = 'none';
-		foodContainer.appendChild(foodName);
+		// const foodName = document.createElement('div');
+		foodContainer.innerHTML = `
+			<div onclick="${'showIngredients()'}">
+				<img id="images" class="image" src="${elements.strMealThumb}" alt="Food Image">
+				<p id="food-name" class="food-name">${elements.strMeal}</p>
+			</div>
+			<div id="ingredients">
+				<p>Ingredients</p>
+				<ul >
+					<li>${elements.strIngredient1}</li>
+					<li>${elements.strIngredient2}</li>
+					<li>${elements.strIngredient3}</li>
+					<li>${elements.strIngredient4}</li>
+					<li>${elements.strIngredient5}</li>
+					<li>${elements.strIngredient6}</li>
+					<li>${elements.strIngredient7}</li>
+					<li>${elements.strIngredient8}</li>
+					<li>${elements.strIngredient9}</li>
+					<li>${elements.strIngredient10}</li>
+				</ul>
+			</div>
+		`;
+		// foodContainer.appendChild(foodName);
 		container.appendChild(foodContainer);
 	});
 };
-
-document.getElementById('images').addEventListener('click', function () {
-	document.getElementsByTagName('ul').style.display = 'none';
-
-	console.log('yes');
-});
+function showIngredients() {
+	document.getElementById('ingredients').style.display = 'block';
+}
