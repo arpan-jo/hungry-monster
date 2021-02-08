5d4d49cd527459067dc5a5f4172f6c3f6c7ca47d
arpan-jo <arpanjorng@gmail.com>
2021-02-08 13:32:38 +0600

    some feature changed

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
+// get foods from search input
 const foodInput = () => {
-	const foodSearch = document.getElementById('search').value;
-	fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodSearch}`)
-		.then(response => response.json())
-		.then(data => showFood(data.meals));
-	document.getElementById('search').value = '';
+  const foodSearch = document.getElementById("searchFood").value;
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodSearch}`)
+    .then(response => response.json())
+    .then(data => showFood(data.meals));
+  document.getElementById("searchFood").value = "";
 };
 
+// show foods after click search
 const showFood = foods => {
-	const container = document.getElementById('food');
-	foods.forEach(elements => {
-		const foodContainer = document.createElement('div');
+  const container = document.getElementById("food");
+  foods.forEach(elements => {
+    const foodContainer = document.createElement("div");
 
-		const foodContainerDiv = `
+    const foodContainerDiv = `
 			<div onclick = "showIngredients('${elements.strMeal}')">
 				<img  class="image" src="${elements.strMealThumb}" alt="Food Image">
 				<p class="food-name">${elements.strMeal}</p>
 			</div>`;
-
-		foodContainer.innerHTML = foodContainerDiv;
-		container.appendChild(foodContainer);
-	});
+    foodContainer.innerHTML = foodContainerDiv;
+    container.appendChild(foodContainer);
+  });
 };
 
+// show ingredients after click specific food item
 const showIngredients = foodName => {
-	fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
-		.then(response => response.json())
-		.then(data => mealIngredientDetails(data.meals[0]));
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+    .then(response => response.json())
+    .then(data => mealIngredientDetails(data.meals[0]));
 };
 const mealIngredientDetails = meals => {
-	console.log(meals);
-	const ingredientDiv = document.getElementById('ingredientDiv');
-	ingredientDiv.innerHTML = `
-		<div class="ingredient-div">
-			<img class="ingredient" src = "${meals.strMealThumb}">
-        <h3>${meals.strMeal}</h3>
-        <h4>Ingredients</h4>
-		  <p>${meals.strIngredient1}</p>
-        <p>${meals.strIngredient2}</p>
-        <p>${meals.strIngredient3}</p>
-        <p>${meals.strIngredient4}</p>
-        <p>${meals.strIngredient5}</p>
-        <p>${meals.strIngredient6}</p>
-        <p>${meals.strIngredient7}</p>
-        <p>${meals.strIngredient8}</p>
-        <p>${meals.strIngredient9}</p>
-        <p>${meals.strIngredient10}</p>
+  console.log(meals);
+  const ingredientDiv = document.getElementById("ingredientDiv");
+  ingredientDiv.innerHTML = `
+		<div class="ingredient-show">
+			<div class="ingredient-div">
+				<p class="food-name">${meals.strMeal}</p>
+				<img class="ingredientImage" src = "${meals.strMealThumb}">	
+			</div>
+			<div>
+				<ol>
+					<li>${meals.strIngredient1}</li>
+					<li>${meals.strIngredient2}</li>
+					<li>${meals.strIngredient3}</li>
+					<li>${meals.strIngredient4}</li>
+					<li>${meals.strIngredient5}</li>
+					<li>${meals.strIngredient6}</li>
+					<li>${meals.strIngredient7}</li>
+					<li>${meals.strIngredient8}</li>
+					<li>${meals.strIngredient9}</li>
+					<li>${meals.strIngredient10}</li>
+				</ol>	
+			</div>
 		</div>
     `;
 };
